9e8da99784b16e981436f852eb5c12a7276c4fb6
Zolton <gmzesq@gmail.com>
2019-08-13 16:52:08 -0400

    Hit second stretch goal - added seed

--- seeds/01-carsDummyData.js
+++ seeds/01-carsDummyData.js
@@ -5,10 +5,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('cars2').insert([
-        {VIN: 'asdfsa'},
-        {make: 'Jeep'},
-        {model: 'Wrangler'},
-        {mileage: 123456}
+        {VIN: 'as22', make: 'Jeep', model: 'Wrangler', mileage: 123456},
+        {VIN: 'ass33', make: 'Jeep', model: 'Wrangler', mileage: 123456},
       ]);
     });
 };
